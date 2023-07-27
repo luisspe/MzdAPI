ce4baf6bedea07d113d64a64602856211bbd1272
luis peña <penagarcia.luisenrique@gmail.com>
2023-07-27 13:45:23 -0600

    client and events api, still need to finish events

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 from .models import Client, Event
-class TodoSerializer(serializers.ModelSerializer):
+class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
         fields = ["email", "name", "number"]
+
+class EventSerializer(serializers.ModelSerializer):
+    client = serializers.PrimaryKeyRelatedField(queryset=Client.objects.all())
+    class Meta:
+        model = Event
+        fields = ["client", "event_type", "event_properties"]
