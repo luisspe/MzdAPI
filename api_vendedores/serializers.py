8804233fbb0587e67c17c77746db52a7d556ffae
luis peña <penagarcia.luisenrique@gmail.com>
2024-02-27 17:40:20 -0600

    fdsfds

--- api_vendedores/serializers.py
+++ api_vendedores/serializers.py
@@ -2,7 +2,7 @@ import uuid
 from rest_framework import serializers
 
 class VendedorSerializer(serializers.Serializer):
-    vendedor_id = serializers.CharField(max_length=40, default=uuid.uuid4)  # Usamos CharField para vendedor_id
+    vendedor_id = serializers.CharField(max_length=40)  # Usamos CharField para vendedor_id
     nombre = serializers.CharField(max_length=200)
     email = serializers.EmailField()
     telefono = serializers.CharField(max_length=15)
@@ -12,12 +12,7 @@ class VendedorSerializer(serializers.Serializer):
     codigo_postal = serializers.CharField(max_length=10)
     sucursal = serializers.CharField(max_length=100)
 
-class UUIDFieldToString(serializers.Field):
-    def to_representation(self, value):
-        return str(value)
-
-    def to_internal_value(self, data):
-        try:
-            return str(uuid.UUID(data))
-        except ValueError:
-            raise serializers.ValidationError("Invalid UUID format.")
+    def create(self, validated_data):
+        # Convierte el UUID a una cadena antes de crear el objeto
+        validated_data['vendedor_id'] = str(uuid.uuid4())
+        return super().create(validated_data)
